3bf1e2f855dafc2f4240019fe5e216ffe96a9a4d
lilianmueninzioki <liliannzioki@gmail.com>
2024-04-04 20:01:23 +0300

    tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const target=document.getElementById('create-task-form');
+  console.log(target);
 });
+
+//add tasks
+const submit=document.getElementById('create-task-form');
+submit.addEventListener('submit',function(e){
+  e.preventDefault();
+  const value=submit.querySelector(input[type='text']).value;
+  console.log(value);
+});
+
+
+const tasks=document.querySelector('#tasks ul');
+//delete tasks
+tasks.addEventListener('click',function(e){
+  if(e.target.id=="list"){
+    const ul=e.target.parentElement;
+    tasks.removeChild(ul);
+  }
+})
